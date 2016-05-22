2b3c322d70ae191a9ad5f95be40c827bc331f419
dingshilun <zgrxdhp@qq.com>
2016-05-22 11:15:07 +0800

    rebuild the website

--- carpool/app.js
+++ carpool/app.js
@@ -4,15 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session')
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
 // view engine setup
+app.use(cookieParser());
+app.use(session(
+  {
+      secret:'landscape',
+      name:'connect.sid',
+      cookie:{maxAge:80000},
+      resave:false,
+      saveUninitialized:true,
+  }
+));
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- carpool/routes/index.js
+++ carpool/routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('login', { title: 'Express' });
+  res.send(req.session.lastPage);
+  req.session.lastPage='from your home2';
+
+});
+
+
+router.get('/home', function(req, res, next) {
+  //res.render('login', { title: 'Express' });
+  res.send(req.session.lastPage);
+  req.session.lastPage='from your home1';
+
 });
 
 module.exports = router;
